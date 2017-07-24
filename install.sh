295456dab35445277711f1cbc112d569d4d33877
Leonard Huang <leihuang.org@gmail.com>
2017-07-24 22:56:38 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -450,10 +450,10 @@ iptables -I INPUT -p tcp --dport 1723 -j ACCEPT
 #gre tunnel protocol
 iptables -I INPUT  --protocol 47 -j ACCEPT
 
-iptables -t nat -A POSTROUTING -s 192.168.2.0/24 -d 0.0.0.0/0 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 172.31.2.0/24 -d 0.0.0.0/0 -o eth0 -j MASQUERADE
 
 #supposedly makes the vpn work better
-iptables -I FORWARD -s 192.168.2.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
+iptables -I FORWARD -s 172.31.2.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
 
 END
 sh /etc/rc.local
@@ -483,8 +483,8 @@ END
 cat >/etc/pptpd.conf <<END
 option /etc/ppp/options.pptpd
 logwtmp
-localip 192.168.2.1
-remoteip 192.168.2.10-100
+localip 172.31.2.1
+remoteip 172.31.2.10-254
 END
 cat >/etc/ppp/options.pptpd <<END
 name pptpd
